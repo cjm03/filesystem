46ea2dc8a67c0a05ccd936add5e6605e0d193854
cjm03 <cjmoye@iu.edu>
2025-11-12 16:25:16 -0500

    cli work

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "tree.h"
@@ -27,38 +26,34 @@ int main(void)
     FSPrintTree(root);
     printf("\n");
 
-    char* choice = malloc(sizeof(char) * 8);
-    char* path = malloc(sizeof(char) * 16);
-    char* def = malloc(sizeof(char) * 16);
-    def = "/";
+    enum { CHOICE_SIZE = 16, PATH_SIZE = 256 };
+    char choice[CHOICE_SIZE];
+    char path[PATH_SIZE];
+    char def[PATH_SIZE];
+    strcpy(def, "/");
     FSNode* cur = root;
-
     while (1) {
         printf("%s > ", def);
-        memset(choice, 0, 6);
-        memset(path, 0, 16);
-        // scanf("%s %s", choice, path);
-        scanf("%s", choice);
-
+        if (scanf("%15s", choice) != 1) {
+            break;
+        }
         if (strncmp(choice, "q", 1) == 0) {
-            free(choice);
-            free(path);
-            free(def);
-            FSFree(root);
-            exit(0);
+            break;
         } else {
-            scanf(" %s", path);
-
+            if (scanf(" %255s", path) != 1) {
+                path[0] = '\0';
+            }
             if (strncmp(choice, "cd", 2) == 0) {
-
-                cur = FSFind(root, path);
-                if (cur) def = path;
-                else printf("cd: %s not a valid path\n", path);
-
+                FSNode* target = FSFind(root, path);
+                if (target) {
+                    cur = target;
+                    strncpy(def, path, PATH_SIZE - 1);
+                    def[PATH_SIZE - 1] = '\0';
+                } else {
+                    printf("cd: %s isnt a valid path\n", path);
+                }
             } else if (strncmp(choice, "ls", 2) == 0) {
-
-                printf("[%s]\n", path);
-                if (strcmp(path, ".") == 0) {
+                if (path[0] == '\0' || strcmp(path, ".") == 0) {
                     PrintLS(cur);
                 } else {
                     FSNode* tols = FSFind(root, path);
@@ -69,10 +64,12 @@ int main(void)
             } else if (strncmp(choice, "mkdir", 5) == 0) {
 
                 n = FSMkdir(root, path);
+                if (!n) fprintf(stderr, "FSMkdir failed for %s\n", path);
 
             } else if (strncmp(choice, "touch", 5) == 0) {
 
                 n = FSCreateFile(root, path, "Placeholder text\n");
+                if (!n) fprintf(stderr, "FSCreateFile failed for %s\n", path);
 
             } else if (strncmp(choice, "cat", 3) == 0) {
 
@@ -92,72 +89,6 @@ int main(void)
         }
     }
 
-
-    free(choice);
-    free(path);
-    free(def);
     FSFree(root);
     return 0;
 }
-
-// int main(void)
-// {
-//     FSNode* root = FSNewRoot();
-//     if (!root) {
-//         fprintf(stderr, "Failed to create root\n");
-//         return 1;
-//     }
-//
-//     FSNode* n = FSMkdir(root, "/dev/disk");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     n = FSMkdir(root, "/dev/usb");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     n = FSMkdir(root, "/etc");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     n = FSMkdir(root, "/sys/kernel");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     n = FSMkdir(root, "/home/user/crab");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     n = FSMkdir(root, "/home/user/guest");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     n = FSMkdir(root, "/home/user/felix");
-//     if (!n) { fprintf(stderr, "FSMkdir failed\n"); FSFree(root); return 1; }
-//     FSNode* pass = FSCreateFile(root, "/etc/passwd", "root:x:0:0:root:/root:/bin/bash\nuser:x:1000:1000::/home/user:/bin/bash\n");
-//     if (!pass) { fprintf(stderr, "FSCreateFile failed\n"); FSFree(root); return 1; }
-//     FSNode* notes = FSCreateFile(root, "/home/user/crab/notes.txt", "Remember: buy milk\n");
-//     if (!notes) { fprintf(stderr, "FSCreateFile failed\n"); FSFree(root); return 1; }
-//     FSNode* log = FSCreateFile(root, "/sys/kernel/log.txt", "Nov 12 17:00 example: started\n");
-//     if (!log) { fprintf(stderr, "FSCreateFile failed\n"); FSFree(root); return 1; }
-//     FSNode* usb = FSCreateFile(root, "/dev/usb/sandisk.txt", "List of things from a usb drive\n");
-//     if (!usb) { fprintf(stderr, "FSCreateFile failed\n"); FSFree(root); return 1; }
-//
-//     printf("Initial tree:\n");
-//     FSPrintTree(root);
-//     printf("\n");
-//
-//     const char* passwd = FSGetFileContent(root, "/etc/passwd");
-//     if (passwd) {
-//         printf("/etc/passwd content:\n%s\n", passwd);
-//     } else {
-//         fprintf(stderr, "failed to read /etc/passwd\n");
-//     }
-//
-//     if (FSMove(root, "/dev/usb/sandisk.txt", "/home/user/guest") == 0) {
-//         printf("Moved /dev/usb/sandisk.txt to /home/user/guest\n");
-//     } else {
-//         printf("Failed to move sandisk.txt\n");
-//     }
-//
-//     if (FSRemove(root, "/dev/usb") == 0) {
-//         printf("Removed /dev/usb\n");
-//     } else {
-//         printf("Failed to remove /dev/usb\n");
-//     }
-//
-//     printf("\nTree after move and remove:\n");
-//     FSPrintTree(root);
-//     printf("\n");
-//
-//     FSFree(root);
-//     return 0;
-// }
